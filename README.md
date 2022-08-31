b1d189bc43c05febf1f7de0b12a6785ead73694d
Geremias Santos <Geremias99999@gmail.com>
2022-08-31 16:24:05 -0300

    Ending To-do List

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 ## Sobre o Mini- Projeto
 
-Projeto de que tem a função de ser um TO-DO list, para organizar rotina e tarefas do dia a dia.
+Projeto de que tem a função de ser um TO-DO list, para organizar rotina e tarefas do dia a dia. Tendo a função de Adicionar novas tares e remove-las.
 
 ## Linguagens Utilizadas
 
--- script.js
+++ script.js
@@ -14,3 +14,58 @@ function showDate(){
 }
 
 showDate() 
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Evento aonde ocorre o fechamento ou remoção de uma tarefa da lista
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Evento aonde ocorre a marcação de check quando atividade é concluída
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Evento aonde ocorre a adição de uma nova tarefa na lista
+function addList() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("O campo está vazio, Insira o que deseja fazer.");
+  } else {
+    document.getElementById("myList").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
